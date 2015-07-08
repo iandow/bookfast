b6102cbcaf3a058f17033f663f8288811996928e
iandow <kayaking@gmail.com>
2015-07-07 22:49:16 -0700

    add table formatting

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,23 +2,10 @@ package controllers
 
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import java.util.Iterator
-import org.jsoup.nodes._
-import scala.collection.JavaConversions._
-
 import org.jsoup
 import org.jsoup.Jsoup
 import org.jsoup.nodes.TextNode
 
-
-import scala.concurrent.Await
-import scala.concurrent.duration.DurationInt
-import play.api.libs.json.Json
-import play.api.libs.ws.WS
-
-import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
@@ -46,19 +33,15 @@ object Application extends Controller {
   }
 
   def recreation = Action {
-
-    def processNode(node: Node) {
-      if (node.isInstanceOf[TextNode]) println(node.toString())
-      node.childNodes() foreach processNode
-    }
-
     val parkid = "75098";
     val formatter = DateTimeFormat.forPattern("MM/dd/yyyy");
-    val date = formatter.parseDateTime("12/23/2015");
-//    val date = DateTime.parse("12-23-2015")
-    val doc = Jsoup.connect("http://www.recreation.gov/campsiteCalendar.do?page=matrix&calarvdate="+date+"&contractCode=NRSO&parkId="+parkid).userAgent("Mozilla/5.0")
+    val date = formatter.parseDateTime("12/29/2015");
+    val url = "http://www.recreation.gov/campsiteCalendar.do?page=matrix&calarvdate="+date.toString("MM/dd/yyyy")+"&contractCode=NRSO&parkId="+parkid
+    System.out.println(url)
+    val doc = Jsoup.connect(url).userAgent("Mozilla/5.0")
     		.timeout(5000).get()
 
+//    System.out.println(doc.body().toString());
     val calendar_elements = doc.select("td[class^=status")
 //    var dailystatus = new Array[String](14)
 //    for (element <- calendar_elements) {
