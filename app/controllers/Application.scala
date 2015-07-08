e93c9bab816f0c889f83f3a51362bb7bae136933
iandow <kayaking@gmail.com>
2015-07-07 22:14:51 -0700

    add test for 5milebutte

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,13 +1,29 @@
 package controllers
 
-import scala.concurrent._
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.util.Iterator
+import org.jsoup.nodes._
+import scala.collection.JavaConversions._
+
+import org.jsoup
+import org.jsoup.Jsoup
+import org.jsoup.nodes.TextNode
+
+
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+import play.api.libs.json.Json
+import play.api.libs.ws.WS
 
 import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.Play.current
+import scala.concurrent.duration.Duration
 import scala.slick.driver.PostgresDriver.simple._
 import models.Site
 import models.Sites
@@ -17,13 +33,41 @@ import models.Subscription
 import models.Subscriptions
 
 object Application extends Controller {
-  	def index = Action {
-		  Ok(views.html.sites.index(Sites.all))
-	}
-  	def availindex = Action {
-		  Ok(views.html.availabilities.index(Availabilities.all))
-	}
+  def index = Action {
+    Ok(views.html.sites.index(Sites.all))
+  }
+
+  def availindex = Action {
+    Ok(views.html.availabilities.index(Availabilities.all))
+  }
+
   def subscriptionindex = Action {
-		  Ok(views.html.subscriptions.index(Subscriptions.all))
-	}
+    Ok(views.html.subscriptions.index(Subscriptions.all))
+  }
+
+  def recreation = Action {
+
+    def processNode(node: Node) {
+      if (node.isInstanceOf[TextNode]) println(node.toString())
+      node.childNodes() foreach processNode
+    }
+
+    val parkid = "75098";
+    val formatter = DateTimeFormat.forPattern("MM/dd/yyyy");
+    val date = formatter.parseDateTime("12/23/2015");
+//    val date = DateTime.parse("12-23-2015")
+    val doc = Jsoup.connect("http://www.recreation.gov/campsiteCalendar.do?page=matrix&calarvdate="+date+"&contractCode=NRSO&parkId="+parkid).userAgent("Mozilla/5.0")
+    		.timeout(5000).get()
+
+    val calendar_elements = doc.select("td[class^=status")
+//    var dailystatus = new Array[String](14)
+//    for (element <- calendar_elements) {
+//      dailystatus[i] = element.owntext()
+//    }
+
+    Ok(views.html.recreation(date, calendar_elements))
+// 		Ok(views.html.recreation(doc.select("td[class^=status").toString))
+// 		Ok(views.html.recreation(doc.select("td[class=status r]").toString))
+ 	}
+
 }
--- build.sbt
+++ build.sbt
@@ -13,7 +13,8 @@ libraryDependencies ++= Seq(
   ws,
   "com.typesafe.slick" %% "slick" % "2.1.0",
   "com.typesafe.play" %% "play-slick" % "0.8.0",
-  "org.postgresql" % "postgresql" % "9.3-1102-jdbc4"
+  "org.postgresql" % "postgresql" % "9.3-1102-jdbc4",
+  "org.jsoup" % "jsoup" % "1.8.2"
 )
 
 
